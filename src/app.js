3f727ede9920ff630e9d680a48761cd95ea7f891
Ciabas <ciabas12@gmail.com>
2017-03-25 22:41:22 +0100

    [init]

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Home from './pages/home/home'
+
+
+let app = document.getElementById('app-root');
+render(
+    <Home/>
+  , app
+);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var path = require('path');
+
+const ROOT_PATH = path.resolve(__dirname);
+const NODE_MODULES_PATH = path.resolve(__dirname, 'node_modules');
+
+const config = {
+  entry: './src/app.js',
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  devServer: { inline: true },
+
+  output: {
+    path: './dist',
+    filename: 'app.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: [NODE_MODULES_PATH],
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
