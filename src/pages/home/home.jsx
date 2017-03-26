fbba314c0fc6385d36b59c642003d800fd2a06f6
Ciabas <ciabas12@gmail.com>
2017-03-26 15:17:19 +0200

    [add]styled-components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import styles from 'styled-components';
 
 export default class Home extends React.Component {
   render () {
     return (
-      <div>
+      <StyledDiv>
         Hello word!
-      </div>
+      </StyledDiv>
     );
   }
 }
+
+const StyledDiv = styles.div`
+  display: block;
+  padding: 6px 12px;
+  border: 1px solid #ddd;
+  text-align: center;
+
+  &:hover{
+    color: red;
+  }
+`
